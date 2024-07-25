b826011ad560cd505806807422ba69edc433b776
RobotLAB <robotlab@RobotLABs-MacBook-Pro-3.local>
2024-07-25 08:19:15 -0700

    Adding the NAO QC file

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Is it raining outside?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Is it raining outside?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Dont forget your umbrella</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Dont forget your umbrella</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Lets go to the beach</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Lets go to the beach</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Done</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Done</translation>
+        </message>
+    </context>
+</TS>
